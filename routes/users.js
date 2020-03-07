8cf3cfdbe115f1a5479dca5ddc5960a98f974309
2dave <2davecollins@gmail.com>
2020-03-07 15:02:50 +0200

    Inital Git Repository Setup

---
+++ routes/users.js
@@ -0,0 +1,131 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcryptjs');
+const passport = require('passport');
+const User = require('../models/User');
+const owasp = require('owasp-password-strength-test');
+const { forwardAuthenticated } = require('../config/auth');
+owasp.config({
+  allowPassphrases       : false,
+  maxLength              : 128,
+  minLength              : 10,
+  minPhraseLength        : 20,
+  minOptionalTestsToPass : 4,
+});
+
+const svgCaptcha = require('svg-captcha');
+
+router.get('/captcha', function (req, res) {
+  var captcha = svgCaptcha.createMathExpr({
+    
+    fontSize: 60,
+    color:true,
+    size: 1,  
+    noise: 3,  
+    width: 100,
+    height: 100,
+    mathOperator:'+',
+    mathMin: 1,
+    mathMax: 4,    
+    
+})
+  req.session.captcha = captcha.text;
+  
+  res.type('svg');
+  res.status(200).send(captcha.data);
+});
+
+// Login Page
+router.get('/login', forwardAuthenticated, (req, res) => res.render('login'));
+
+// Register Page
+router.get('/register', forwardAuthenticated, (req, res) => res.render('register'));
+
+// Register
+router.post('/register', (req, res) => {
+  const { name, email, password, password2 } = req.body;
+  const owaspcheck = owasp.test(password);
+  let errors = []; 
+
+  if (!name || !email || !password || !password2) {
+    errors.push({ msg: 'Please enter all fields' });
+  }
+
+  if (password != password2) {
+    errors.push({ msg: 'Passwords do not match' });
+  }
+  if(owaspcheck.errors.length > 0){
+    if(owaspcheck.failedTests.length > 0){
+      errors.push({msg: "OSWAP SAYS NOT Good Enough\n\t\t"+owaspcheck.errors[0]});
+    }
+   
+  }
+
+  if (password.length < 6) {
+    errors.push({ msg: 'Password must be at least 6 characters' });
+  }
+
+  if (errors.length > 0) {
+    res.render('register', {
+      errors,
+      name,
+      email,
+      password,
+      password2
+    });
+  } else {
+    User.findOne({ email: email }).then(user => {
+      if (user) {
+        errors.push({ msg: 'Email already exists' });
+        res.render('register', {
+          errors,
+          name,
+          email,
+          password,
+          password2
+        });
+      } else {
+        const newUser = new User({
+          name,
+          email,
+          password
+        });
+
+        bcrypt.genSalt(10, (err, salt) => {
+          bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if (err) throw err;
+            newUser.password = hash;
+            newUser
+              .save()
+              .then(user => {
+                req.flash(
+                  'success_msg',
+                  'You are now registered and can log in'
+                );
+                res.redirect('/users/login');
+              })
+              .catch(err => console.log(err));
+          });
+        });
+      }
+    });
+  }
+});
+
+// Login
+router.post('/login', (req, res, next) => {
+  passport.authenticate('local', {
+    successRedirect: '/dashboard',
+    failureRedirect: '/users/login',
+    failureFlash: true
+  })(req, res, next);
+});
+
+// Logout
+router.get('/logout', (req, res) => {
+  req.logout();
+  req.flash('success_msg', 'You are logged out');
+  res.redirect('/users/login');
+});
+
+module.exports = router;
