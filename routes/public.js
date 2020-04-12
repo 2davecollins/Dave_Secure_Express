d8a983bcf9daaac69bdd1db5ef3c1200228c893d
2dave <2davecollins@gmail.com>
2020-04-12 12:44:52 +0300

    merge branch

--- routes/public.js
+++ routes/public.js
@@ -1,7 +1,17 @@
 const express = require('express'),
 	router = express.Router();
 
+const ErrorResponse = require('../utility/errorResponse');
+
 router.get('/blog', (req, res) => {
+	const c = false;
+	console.log(`in blog ${c}`);
+	
+	if(c == false){
+		console.log("error response");
+		const message = `Resource not found`;
+		error = new ErrorResponse(message, 404);
+	}
 	res.render('blog');
 });
 
